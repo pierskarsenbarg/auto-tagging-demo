e3ba330f74f5aad026e38ec6ff3779e03cca0323
Piers Karsenbarg <piers@pulumi.com>
2022-02-22 16:09:41 +0000

    Updated logging

--- autotag.ts
+++ autotag.ts
@@ -10,7 +10,7 @@ export function registerAutoTags(autoTags: Record<string, string>): void {
         if (isTaggable(args.type)) {
             pulumi.log.info(`Type: ${args.type}`);
             args.props["tags"] = { ...args.props["tags"], ...autoTags };
-            pulumi.log.info(`props: ${JSON.stringify(args.props["tags"])}`)
+            pulumi.log.info(`Tags: ${JSON.stringify(args.props["tags"])}`)
             return { props: args.props, opts: args.opts };
         }
         return undefined;
